10556b8ac3161ffa29e61e8fcdee161e3c05a54e
Steven <122954695+Vengxdor@users.noreply.github.com>
2023-05-19 11:50:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const info = document.querySelector(".mobile__info")
+const burger = document.getElementById("burger")
+
+
+burger.addEventListener("click", () =>{
+  info.classList.toggle("diseable")
+  info
+})
